a4718808f4cf48247e9b6207c677f5563441c999
Dean Church <dean.church@tooeleschools.org>
2019-09-04 14:10:02 -0600

    first push

---
+++ AboutMe/src/AboutMe/AboutMe.java
@@ -0,0 +1,26 @@
+package AboutMe;
+
+public class AboutMe {
+
+	public AboutMe() {
+		String period = "period";
+		System.out.println("Dean Church Schedule");
+		System.out.println("period 1 Physics 8:00 - 9:10");
+		System.out.println("period 2 French 9:15 to 10:25");
+		System.out.println("period 3 English 10:30 - 11:40");
+		System.out.println("period 4 Java 12:15 - 2:20");
+		System.out.println("period 5 Java");
+		System.out.println("period 6 World War 2");
+		System.out.println("period 7 U.S History");
+		System.out.println("period 8 Math");
+		System.out.println("period 9 AP studio design");
+		System.out.println("period 10 Personal Finance");
+		
+	}
+
+	public static void main(String[] args) {
+		new AboutMe();
+
+	}
+
+}
